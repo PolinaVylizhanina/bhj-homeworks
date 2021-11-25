cee6d540db203c19709827718ed692803e43dea1
Polina Vylizhanina <vylizhaninaps@gmail.com>
2021-11-25 18:10:04 +0500

    popup task solution

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,6 +1,6 @@
-const modalMain = document.getElementById('modal_main');
-const closeModal = document.querySelectorAll('.modal__close');
-const modalSuccess = document.getElementById('modal_success');
+const modalMain = document.getElementById('modal_main')
+const closeModal = document.querySelectorAll('.modal__close')
+const modalSuccess = document.getElementById('modal_success')
 const allModals = document.querySelectorAll('.modal')
 
 
@@ -11,8 +11,10 @@ closeModal.forEach(element => {
     element.onclick = function() {
         
         if (element.classList.contains('show-success')) {
-            modalSuccess.classList.add('modal_active');
+
+            modalSuccess.classList.add('modal_active')
             modalMain.classList.remove('modal_active')
+            
         } else {
             allModals.forEach( element => {
                 element.classList.remove('modal_active')
